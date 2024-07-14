97bbc9330e1d350f4fcb9279c523e4ec001d2689
Nasreena <syednasreen1506@gmail.com>
2024-07-14 11:57:36 +0530

    updated

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -1,26 +1,26 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
    
-    function showGallery(selectedGalleryId) {
-      $w('#gridGallery1').hide();
-      $w('#gridGallery2').hide();
-      $w('#gridGallery3').hide();
-      $w('#gridGallery4').hide();
+  function showGallery(selectedGalleryId) {
+    $w('#gridGallery1').hide();
+    $w('#gridGallery2').hide();
+    $w('#gridGallery3').hide();
+    $w('#gridGallery4').hide();
 
-      $w(selectedGalleryId).show();
-    }
-    $w("#text55").onClick(() => {
-      showGallery('#gridGallery2');
-    });
-  
-    $w("#text56").onClick(() => {
-      showGallery('#gridGallery3');
-    });
-  
-    $w("#text57").onClick(() => {
-      showGallery('#gridGallery4');
-    });
-  
+    setTimeout(() => {
+      $w(selectedGalleryId).show("fade", { duration: 200 });
+  }, 1200);
+  }
+  $w("#text55").onClick(() => {
+    showGallery('#gridGallery2');
   });
+
+  $w("#text56").onClick(() => {
+    showGallery('#gridGallery3');
+  });
+
+  $w("#text57").onClick(() => {
+    showGallery('#gridGallery4');
+  });
+});  
+
